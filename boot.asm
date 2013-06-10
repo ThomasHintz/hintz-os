6f67b4b5c38501f787eba0213832a515c2ad3aaa
thintz <thintz@hotpads.com>
2013-06-10 08:07:55 -0700

    Remove second print.

--- boot.asm
+++ boot.asm
@@ -16,27 +16,11 @@
 print_loop:
         lodsb                   ; Load string.
         or al, al               ; zero = end of string
-        jz print_loop2          ; Jump to next print when result of
+        jz hang                 ; Jump to next print when result of
                                 ; previous instruction is equal to 0,
                                 ; which is the end of the string.
         mov ah, 0x0E
         int 0x10                ; Output interrupt.
-        jmp print_loop
-
-
-
-	    xor ax, ax
-        mov ds, ax
-
-        mov si, msg2
-
-print_loop2:
-        lodsb
-        or al, al
-        jz hang                 ; Jump to hang when result of previous
-                                ; instruction is equal to 0.
-        mov ah, 0x0E
-        int 0x10
         jmp print_loop
 
 
@@ -45,7 +29,6 @@ hang:
 
 
 msg:    db 'Hello world.', 13, 10, 0
-msg2:   db 'Hello world2.', 13, 10, 0
 
         times 510 - ($-$$) db 0
 
