7e464f66c27fdfa9b2a62ce5277fc973de4e0b69
thintz <thintz@hotpads.com>
2013-06-13 12:01:47 -0700

    wip

--- boot.asm
+++ boot.asm
@@ -51,13 +51,58 @@ printc:
 hang:
         jmp hang
 
+;; AH = 02h
+;; AL = number of sectors to read (must be nonzero)
+;; CH = low eight bits of cylinder number
+;; CL = sector number 1-63 (bits 0-5)
+;; high two bits of cylinder (bits 6-7, hard disk only)
+;; DH = head number
+;; DL = drive number (bit 7 set for hard disk)
+;; ES:BX -> data buffer
+;;
+;; Return: CF set on error if AH = 11h (corrected ECC error), AL =
+;; burst length CF clear if successful AH = status (see #00234) AL =
+;; number of sectors transferred (only valid if CF set for some
+;; BIOSes)
+
+getsector:
+        mov ax, [RawSecNum]
+        mov dl, [DrvNum]        ; set drive number
+
+        mov ax, 0x0201          ; command to read 1 sector
+        int 0x13                ; read sector
+
+findkernel:
+
+        ;; need to get the start of the root sector
+        mov al, "f"             ; find kernel
+        call printc
+
+        call getsector
+
+        ret
+
 bootstart:
 
-        mov al, "e"
+        cld
+
+        mov al, "s"             ; start the process
         call printc
+
+        mov ax, [SectPerTrack]
+        mul byte [NumHeads]
+        mov [SecsPerCyl], al
+
+        call findkernel
 
         jmp hang
 
 	    times 509 - $ + boottop db 0 ; Space out to offset 508
 	    db GeometryTableSize         ; Bytes in geometry table
         db 0x55, 0xaa
+
+section .bss
+        RootDirSectorStart resw 1 ; Sector root dir starts at.
+        RootDirNumSectors resw 1  ; Sectors in root dir.
+        RawSecNum resw 1
+        SecsPerCyl resw 1
